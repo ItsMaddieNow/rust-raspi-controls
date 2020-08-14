86ea7e2c4eca5c109e86b8d16acb278a609d1a26
Mario Whitmore <mariolwhitmore@gmail.com>
2020-08-14 18:16:13 +1200

    Reading from joystick implemented. I have no idea what the hell I'm doing with rust but it seems to be working and efficient.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,38 @@
-//use std::error::Error;
-//use std::thread;
-//use std::time::Duration;
-use std::str::FromStr;
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
 
-use mcp3xxx::{Channel, Mcp3008};
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
 
-//use rppal::gpio::Gpio;
-use rppal::spi::{Bus, Mode/*, Segment*/, SlaveSelect, Spi};
+use mcp3008::Mcp3008;
 
-const SPI_CLOCK_SPEED: u32 = 2000000;
+const GPIO_LED: u8 = 23;
+
+//const SPI_CLOCK_SPEED: u32 = 1_000_000;
 const XCHANNEL: u8 = 1;
 const YCHANNEL: u8 = 2;
 const BUTTONCHANNEL: u8 = 0;
 
 
-<<<<<<< HEAD
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
 
-    let mut mcp3008 = Mcp3008::new(Spi::new(Bus::Spi0, SlaveSelect::Ss0, SPI_CLOCK_SPEED, Mode::Mode0).unwrap()).unwrap();
+   
 
-    let args = std::env::args().collect::<Vec<String>>();
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+    let mut mcp3008 = Mcp3008::new("/dev/spidev0.0").unwrap();
 
-    let result = mcp3008.single_ended_read(Channel::new(XCHANNEL).unwrap()).unwrap();
-    
-    println!(
-        "{}/{} = {}",
-        result.value(),
-        result.range(),
-        result.as_fraction()
-    );
-    //let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
-
-    // Blink the LED by setting the pin's logic level high for 500 ms.
-    /*loop{
-        pin.set_high();
-        thread::sleep(Duration::from_millis(500));
-        pin.set_low();
-    }*/
-=======
     // Blink the LED by setting the pin's logic level high for 500 ms.
     loop{
+        println!("Button {}", mcp3008.read_adc(BUTTONCHANNEL).unwrap());
+        println!("X Co-ord {}", mcp3008.read_adc(XCHANNEL).unwrap());
+        println!("Y Co-ord {}", mcp3008.read_adc(YCHANNEL).unwrap());
+
         pin.set_high();
         thread::sleep(Duration::from_millis(500));
         pin.set_low();
         thread::sleep(Duration::from_millis(500));
     }
->>>>>>> Blinking Led
     //Ok(())
 }
